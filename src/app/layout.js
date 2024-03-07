45c53aa2fa853c2436ab167665bfa122912e98cb
LurchingDart <luizalferez25@gmail.com>
2024-03-07 12:13:50 -0600

    Font groteska eliminated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,45 +1,8 @@
-import { Groteska, KodeMono } from "next/font/local"
+import localFont from 'next/font/local'
 import "./globals.css";
 import { cn } from "@/utils/cn";
 
-const fontGroteska = Groteska({
-  src: [
-    {
-      path: '../../public/fonts/Groteska-Thin.woff2',
-      weight: '100',
-      style: 'light',
-    },
-    {
-      path: '../../public/fonts/Groteska-Light.woff2',
-      weight: '300',
-      style: 'thin',
-    },
-    {
-      path: '../../public/fonts/Groteska-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../../public/fonts/Groteska-Medium.woff2',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../../public/fonts/Groteska-Bold.woff2',
-      weight: '700',
-      style: 'bold',
-    },
-    {
-      path: '../../public/fonts/Groteska-Black.woff2',
-      weight: '900',
-      style: 'black',
-    },
-  ],
-  display: "swap",
-  variable: "--font-groteska",
-})
-
-const fontKodeMono = KodeMono({
+const fontKodeMono = localFont({
   src: "../../public/fonts/KodeMono-VariableFont_wght.ttf",
   display: "swap",
   variable: "--font-kode",
@@ -56,7 +19,7 @@ export default function RootLayout({ children }) {
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          `${fontGroteska.variable} ${fontKodeMono.variable}`
+          `${fontKodeMono.variable}`
         )}
       >{children}</body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen">
+      <h1 className="text-4xl font-kode ">Welcome to Blosthex</h1>
     </main>
   );
 }
