572b98f5c1e02f28c68e10d6362bbd6d1b29d846
Luis Alférez <lurchdev@gmail.com>
2024-03-09 16:33:35 -0600

    Refactor button styles

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -5,24 +5,28 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-zinc-300",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium focus-visible:ring-6 focus-visible:ring-accent-default",
   {
     variants: {
       variant: {
         default:
-          "bg-zinc-900 text-zinc-50 shadow hover:bg-zinc-900/90 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90",
-        destructive:
-          "bg-red-500 text-zinc-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-zinc-50 dark:hover:bg-red-900/90",
-        outline:
-          "border border-zinc-200 bg-white shadow-sm hover:bg-zinc-100 hover:text-zinc-900 dark:border-zinc-800 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
+          "bg-primary-default text-text-950 hover:bg-primary-default/90 hover:shadow-sm hover:shadow-accent-default",
         secondary:
-          "bg-zinc-100 text-zinc-900 shadow-sm hover:bg-zinc-100/80 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-800/80",
+          "bg-secondary-800 hover:bg-secondary-800/80 hover:shadow-sm hover:shadow-accent-default",
+        confirmative:
+          "bg-green-500 shadow-sm hover:bg-green-500/90 ",
+        warning:
+            "bg-yellow-500 shadow-sm hover:bg-yellow-500/90 ",
+        destructive:
+          "bg-red-500 shadow-sm hover:bg-red-500/90 ",
+        outline:
+          "border border-primary-default hover:border-primary-default/80 hover:shadow-accent-default",
         ghost:
-            "hover:bg-zinc-100 hover:text-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
+          "hover:bg-background-100 hover:text-text-950",
         link:
-            "text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50",
+          "underline-offset-4 hover:underline",
         card:
-            "text-text-950 text-lg bg-background-50 shadow-lg shadow-background-700",
+          "text-text-950 text-lg bg-background-50 shadow-lg shadow-background-700",
       },
       size: {
         default: "h-9 px-4 py-2",
