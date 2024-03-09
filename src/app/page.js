22e62b5215a3411db40844d3e410dd6efa0e9a41
Luis Alférez <lurchdev@gmail.com>
2024-03-08 21:24:41 -0600

    Hero section
    Falta por terminar el efecto 3D de las tarjetas o encontrar la forma de usar una mascara con un svg pero que se pegue a los bordes del path y no de un rectangulo

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,50 @@
 export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <h1 className="text-4xl font-kode ">Welcome to Blosthex</h1>
-    </main>
-  );
+    return (
+        <main className="min-h-screen">
+            <section className="h-screen">
+                <h1 className="text-text-50 text-[192px] font-bold text-center">
+                    NEW ERA OF
+                </h1>
+                <h1 className="text-primary-default text-[192px] font-bold text-center">
+                    BANKING
+                </h1>
+                <div className="relative flex justify-center">
+                    <div
+                        className="w-[450px] h-[290px] z-10 left-40 -top-6 relative rotate-[-76.81deg] bg-background-50 bg-opacity-10 rounded-[40px] shadow-lg shadow-background-950 border-l-4 border-b-4 border-background-50 border-opacity-30 backdrop-blur-[5px]">
+                        <span
+                            className="w-[328.03px] h-[68.89px] left-[59.27px] top-[201.67px] absolute text-slate-100 text-2xl font-medium font-['Kode Mono']">Luis
+                            Alferez
+                        </span>
+                        <span
+                            className="w-[279.89px] h-[68.89px] left-[57.48px] top-[141.55px] absolute text-slate-100 text-2xl font-medium font-['Kode Mono']">****
+                            **** **** ****
+                        </span>
+                    </div>
+                    <div
+                        className="w-[450px] h-[290px] top-[20vh] -left-40 relative rotate-[-76.81deg] bg-background-50 bg-opacity-10 rounded-[40px] shadow-lg shadow-background-950 border-l-4 border-b-4 border-background-50 border-opacity-30">
+                        <span
+                            className="w-[328.03px] h-[68.89px] left-[59.27px] top-[201.67px] absolute text-slate-100 text-2xl font-medium font-['Kode Mono']">Yamile
+                            Robledo
+                        </span>
+                        <span
+                            className="w-[279.89px] h-[68.89px] left-[57.48px] top-[141.55px] absolute text-slate-100 text-2xl font-medium font-['Kode Mono']">****
+                            **** **** ****
+                        </span>
+                    </div>
+                    <div
+                        className="w-[450px] h-[290px] top-[10vh] absolute rotate-[-76.81deg] bg-background-50 bg-opacity-10 rounded-[40px] shadow-lg shadow-background-950 border-l-4 border-b-4 border-background-50 border-opacity-30 backdrop-blur-[5px]">
+                        <span
+                            className="w-[328.03px] h-[68.89px] left-[59.27px] top-[201.67px] relative text-slate-100 text-2xl font-medium font-['Kode Mono']">David
+                            Martínez
+                        </span>
+                        <span
+                            className="w-[279.89px] h-[68.89px] left-[57.48px] top-[141.55px] absolute text-slate-100 text-2xl font-medium font-['Kode Mono']">****
+                            **** **** ****
+                        </span>
+                    </div>
+                </div>
+            </section>
+
+        </main>
+    );
 }
