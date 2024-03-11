6adcb44185230bd95e20099ed618e2e8890a3a9d
Luis Alférez <lurchdev@gmail.com>
2024-03-11 17:35:02 -0600

    Refactor SignUp page layout and add SignUpForm component

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,20 +1,25 @@
 'use client';
+import SignUpForm from "@/components/forms/SignUpForm";
 import Image from "next/image";
 
 
 export default function SignUp() {
     return (
-        <main className="flexs h-screen items-center border-2 border-primary-200 px-40 flex-grow gap-32">
-            <div className="w-3/5 border-2 border-red-500">
-            </div>
-            <div className="flex flex-col w-2/5 items-center border-2 border-accent-400">
-                <Image src="/images/bank.svg" alt="Bank image" width={262} height={200}/>
-                <h1 className="text-center text-6xl font-bold leading-[96px]">
-                    You are joining the new
-                </h1>
-                <h1 className="text-center text-primary-default text-8xl leading-[144px]">
-                    ERA
-                </h1>
+        <main className="flex flex-col items-center justify-center h-svh p-5">
+            <h2 className="text-[32px] leading-[48px] mb-auto">
+                Blosthex
+            </h2>
+            <div className="flex items-center px-40 gap-32 mb-auto">
+                <SignUpForm />
+                <div className="flex flex-col w-2/5 items-center">
+                    <Image src="/images/bank.svg" alt="Bank image" width={262} height={200} />
+                    <h1 className="text-center text-6xl font-bold leading-[96px]">
+                        You are joining the new
+                    </h1>
+                    <h1 className="text-center text-primary-default text-8xl">
+                        ERA
+                    </h1>
+                </div>
             </div>
         </main>
     );
