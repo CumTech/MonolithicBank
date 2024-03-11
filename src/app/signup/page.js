e67d9d58d49c5df0c0cf6d3cb11a8d06076ea214
Luis Alférez <lurchdev@gmail.com>
2024-03-11 16:52:08 -0600

    Refactor signup page layout and add bank image

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,7 +1,21 @@
-export default function NotFound() {
+'use client';
+import Image from "next/image";
+
+
+export default function SignUp() {
     return (
-        <div className="flex h-screen justify-center items-center">
-            <h2>Page</h2>
-        </div>
-    )
+        <main className="flexs h-screen items-center border-2 border-primary-200 px-40 flex-grow gap-32">
+            <div className="w-3/5 border-2 border-red-500">
+            </div>
+            <div className="flex flex-col w-2/5 items-center border-2 border-accent-400">
+                <Image src="/images/bank.svg" alt="Bank image" width={262} height={200}/>
+                <h1 className="text-center text-6xl font-bold leading-[96px]">
+                    You are joining the new
+                </h1>
+                <h1 className="text-center text-primary-default text-8xl leading-[144px]">
+                    ERA
+                </h1>
+            </div>
+        </main>
+    );
 }
