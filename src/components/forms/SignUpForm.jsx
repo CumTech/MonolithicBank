a2afbfd725739e5e8ad3382c84b8b55b0743e83a
Luis Alférez <lurchdev@gmail.com>
2024-03-11 17:34:56 -0600

    Fix formatting in SignUpForm component

--- src/components/forms/SignUpForm.jsx
+++ src/components/forms/SignUpForm.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 
 const validationSchema = Yup.object().shape({
     name: Yup.string()
@@ -22,7 +22,11 @@ const validationSchema = Yup.object().shape({
 
 export default function SignUpForm() {
     return (
-        <div className="flex h-screen justify-center items-center">
+        <div className="w-3/5 flex flex-col gap-5">
+            <div>
+                <h2 className="text-primary-default text-2xl leading-9">START FOR FREE</h2>
+                <h2 className="text-[64px] leading-[96px]">Create new account</h2>
+            </div>
             <Formik
                 initialValues={{ name: '', lastName: '', email: '', password: '', curp: '' }}
                 validationSchema={validationSchema}
@@ -31,50 +35,44 @@ export default function SignUpForm() {
                 }}
             >
                 {() => (
-                    <Form className="flex flex-col gap-4">
+                    <Form className="flex flex-col gap-9">
                         <div className="flex flex-row gap-4">
-                            <div className="flex flex-col gap-2">
-                                <label>
-                                    Name
-                                    <Field name="name" type="text" placeholder="Nombre"
-                                           className="w-full p-3 rounded-xl border border-secondary-300 bg-transparent"/>
-                                </label>
-                                <ErrorMessage name="name" component="div" className="text-danger-400/90"/>
+                            <div className="flex flex-col w-1/2 gap-2">
+                                <Field
+                                    name="name"
+                                    type="text"
+                                    placeholder="Nombre"
+                                    className="w-full p-3 rounded-xl border border-secondary-300 bg-transparent" />
+                                <ErrorMessage name="name" component="div" className="text-danger-400/90" />
                             </div>
-                            <div className="flex flex-col gap-2">
+                            <div className="flex flex-col w-1/2 gap-2">
                                 <label>
-                                    Last Name
                                     <Field name="lastName" type="text" placeholder="Apellido"
-                                           className="w-full p-3 rounded-xl border border-secondary-300 bg-transparent"/>
+                                        className="w-full p-3 rounded-xl border border-secondary-300 bg-transparent" />
                                 </label>
-                                <ErrorMessage name="lastName" component="div" className="text-danger-400/90"/>
+                                <ErrorMessage name="lastName" component="div" className="text-danger-400/90" />
                             </div>
                         </div>
 
-                        <label>
-                            Email
+                        <div className="flex flex-col gap-2">
                             <Field name="email" type="email" placeholder="Email"
-                                   className="w-full p-3 rounded-xl border border-secondary-300 bg-transparent"/>
-                        </label>
-                        <ErrorMessage name="email" component="div" className="text-danger-400/90"/>
+                                className="w-full p-3 rounded-xl border border-secondary-300 bg-transparent" />
+                            <ErrorMessage name="email" component="div" className="text-danger-400/90" />
+                        </div>
 
-                        <label>
-                            Password
+                        <div className="flex flex-col gap-2">
                             <Field name="password" type="password" placeholder="Contraseña"
-                                   className="w-full p-3 rounded-xl border border-secondary-300 bg-transparent"/>
-                        </label>
-                        <ErrorMessage name="password" component="div" className="text-danger-400/90" />
+                                className="w-full p-3 rounded-xl border border-secondary-300 bg-transparent" />
+                            <ErrorMessage name="password" component="div" className="text-danger-400/90" />
+                        </div>
 
-                        <label>
-                            CURP
+                        <div className="flex flex-col gap-2">
                             <Field name="curp" type="text" placeholder="CURP" className="w-full p-3 rounded-xl border border-secondary-300 bg-transparent" />
-                        </label>
-                        <ErrorMessage name="curp" component="div" className="text-danger-400/90" />
-
+                            <ErrorMessage name="curp" component="div" className="text-danger-400/90" />
+                        </div>
                         <div className="flex justify-center">
                             <Button
                                 type="submit"
-                                className="w-fit"
                             >
                                 Create Account
                             </Button>
