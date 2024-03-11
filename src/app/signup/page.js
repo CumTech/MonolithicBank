51a434a02060f62f279b72575f0b34cbf888943e
Luis Alférez <lurchdev@gmail.com>
2024-03-11 17:40:58 -0600

    Refactor signup page layout and add new components

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,7 +1,26 @@
-export default function NotFound() {
+'use client';
+import SignUpForm from "@/components/forms/SignUpForm";
+import Image from "next/image";
+
+
+export default function SignUp() {
     return (
-        <div className="flex h-screen justify-center items-center">
-            <h2>Page</h2>
-        </div>
-    )
+        <main className="flex flex-col items-center justify-center h-svh p-5">
+            <h2 className="text-[32px] leading-[48px] mb-auto">
+                Blosthex
+            </h2>
+            <div className="flex items-center px-40 gap-32 mb-auto">
+                <SignUpForm />
+                <div className="flex flex-col w-2/5 items-center">
+                    <Image src="/images/bank.svg" alt="Bank image" width={262} height={200} />
+                    <h1 className="text-center text-6xl font-bold leading-[96px]">
+                        You are joining the new
+                    </h1>
+                    <h1 className="text-center text-primary-default text-8xl">
+                        ERA
+                    </h1>
+                </div>
+            </div>
+        </main>
+    );
 }
