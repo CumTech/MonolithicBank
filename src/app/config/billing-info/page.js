e0bf1763e6b2153d1b882e771431dd0ac76ab2a3
David Emmanuel Cerda Martinez <davidmartinez5732891@gmail.com>
2024-03-24 20:46:45 -0600

    The billing-info was prepareted to appear, It was not have the correct text and styles, but it works

--- src/app/config/billing-info/page.js
+++ src/app/config/billing-info/page.js
@@ -1,8 +1,7 @@
 import SidebarConfig, { SidebarItemConfig } from "@/components/SideBarConfig";
 import Image from "next/image"
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { Switch } from "@/components/ui/switch"
+import Link from "next/link";
 import {
     AlertDialog,
     AlertDialogAction,
@@ -24,14 +23,13 @@ export default function BillingInfo() {
                     <SidebarItemConfig icon={ <Image src="/icons/user.svg" alt="User Icon" width={18} height={18} /> } text="User Info" url={`/config/user-info`}/>
                     <SidebarItemConfig icon={ <Image src="/icons/lock.svg" alt="Password Icon" width={18} height={18} /> } text="Password" url={`/config/password`} />
                     <SidebarItemConfig icon={ <Image src="/icons/info.svg" alt="Information Icon" width={18} height={18} /> } text="Personal Info" url={`/config/address-info`} />
-                    <SidebarItemConfig icon={ <Image src="/icons/credit-card-config.svg" alt="Credit Car Config Icon" width={18} height={18} /> } text="Billing" url={`/config/billing-info`} active={true} />
-                    <li className="relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group bg-background-200/10 text-text-50/50 hover:bg-background-50 hover:text-text-950">
+                    <li className="relative flex items-center py-3 px-3 my-0 font-medium rounded-md cursor-pointer transition-colors group hover:bg-background-50 hover:text-text-950 text-text-50/50">
                         <Image src="/icons/credit-card-config.svg" alt="Credit Car Config Icon" width={18} height={18} />
                         <Link
                             href="/config/billing-info"
                             className="overflow-hidden transition-all w-52 ml-3">
                             <AlertDialog>
-                                <AlertDialogTrigger>Billing</AlertDialogTrigger>
+                                <AlertDialogTrigger className="w-full h-full my-0 top-0 left-0 absolute">Billing</AlertDialogTrigger>
                                 <AlertDialogContent>
                                     <AlertDialogHeader>
                                         <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
@@ -49,14 +47,6 @@ export default function BillingInfo() {
                         </Link>
                     </li>
                 </SidebarConfig>
-            </div>
-            <div className="flex grow my-10 mr-10">
-                <div className="flex items-center justify-center  w-full h-full rounded-[30px] bg-background-50/10">
-                    <h1 className="text-2xl font-semibold">
-                    
-                        <Switch />
-                    </h1>
-                </div>
             </div>
         </div>
     )
